a83ec51ee2d8a4af42be0122e6949078d9d1d04e
AlbertoScalia <albscalia@gmail.com>
2023-04-19 17:16:48 +0200

    Fix (Javascript) : Fixed name

--- Js/script.js
+++ Js/script.js
@@ -11,7 +11,7 @@ const slides = [
 const btnDown = document.getElementById(`down`);
 const btnUp = document.getElementById(`up`);
 const slideContainer = document.getElementById(`slide-container`)
-let counterSLide = 0;
+let counterSlide = 0;
 
 //Avvio ciclo for per cambiare immagini
 for (let i = 0; i < slides.length; i++) {
@@ -21,27 +21,27 @@ for (let i = 0; i < slides.length; i++) {
 }
 
 const slideSet = document.getElementsByClassName(`slide`);
-slideSet[counterSLide].classList.remove(`d-none`)
+slideSet[counterSlide].classList.remove(`d-none`)
 
 
 //addEventListener per attivare il ciclo infinito col bottone in alto
 btnUp.addEventListener(`click`, function () {
-  slideSet[counterSLide].classList.add(`d-none`);
-  if (counterSLide === 0) {
-    counterSLide = slides.length;
+  slideSet[counterSlide].classList.add(`d-none`);
+  if (counterSlide === 0) {
+    counterSlide = slides.length;
   }
-  counterSLide--;
-  slideSet[counterSLide].classList.remove(`d-none`);
+  counterSlide--;
+  slideSet[counterSlide].classList.remove(`d-none`);
 });
 
 //addEventListener per attivare il bottone in basso
 btnDown.addEventListener(`click`, function () {
-  slideSet[counterSLide].classList.add(`d-none`);
-  counterSLide++;
-  if (counterSLide === slides.length) {
-    counterSLide = 0;
+  slideSet[counterSlide].classList.add(`d-none`);
+  counterSlide++;
+  if (counterSlide === slides.length) {
+    counterSlide = 0;
   }
-  slideSet[counterSLide].classList.remove(`d-none`);
+  slideSet[counterSlide].classList.remove(`d-none`);
 });
 
 
